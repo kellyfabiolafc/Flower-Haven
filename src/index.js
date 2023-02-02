bce4d27cf52d2712cf24bb8a39e5e67822e9400e
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2023-02-01 19:30:29 -0500

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
+
 import validator from './validator.js';
 
-console.log(validator);
+const input = document.getElementById("cardnumber");
+input.addEventListener("input", function() {
+  if (!/^\d+$/.test(this.value)) {
+    // El valor ingresado no es un número, se elimina
+    this.value = this.value.replace(/[^\d]+/g, "");
+    alert("solo puedes ingresar numeros");
+  }
+});
+
+const form = document.getElementById("myForm");
+form.addEventListener("submit", function(event) {
+  const input = document.getElementById("cardnumber");
+  if (input.value.trim().length === 0) {
+    // El campo está vacío, se detiene el envío del formulario
+    event.preventDefault();
+    alert("El campo no puede estar vacío");
+  }
+  /*else { 
+    if (input.value.trim().length < 14 ) {
+      event.preventDefault();
+      alert("El campo no puede tener menos de 14 caracteres");
+    }*/
+  else {
+    const valid = validator.isValid(document.getElementById("cardnumber").value);
+    const maskify=validator.maskify(document.getElementById("cardnumber").value);
+    event.preventDefault();
+    if (valid){  
+      // Show success in div#result
+      alert("Genial, el número de  tarjeta insertada  es correcta " )
+      document.getElementById("result").style.display="block";
+      document.getElementById("maskify").style.display="block";
+      document.getElementById("result").innerHTML = 'Tarjeta valida: ';
+      document.getElementById("maskify").innerHTML = 'Número enmascarado: '+ maskify;
+    }
+    else {
+      // Show error message in div#result
+      alert( "Lo sentimos,intente con otro número de tarjeta" );
+      document.getElementById("result").style.display="block";
+      document.getElementById("maskify").style.display="block";
+      document.getElementById("result").innerHTML = 'Tarjeta invalida: ';
+      document.getElementById("maskify").innerHTML = 'Número enmascarado: '+ maskify;
+    }
+  }
+  //}
+});
+
+ 
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,47 @@
 const validator = {
-  // ...
-};
-
+  isValid: (creditCardNumber) => {
+    function invertirCadena(creditCardNumber) {
+      if (creditCardNumber === "")
+        return "";
+      else
+        return invertirCadena(creditCardNumber.substr(1)) + creditCardNumber.charAt(0);
+    }
+    const reverseCreditCardNumber=invertirCadena(creditCardNumber);
+    const amountOfTours=creditCardNumber.length;
+    let SumaValoresdePocisionesPares=0;
+    let SumaValoresdePocisionesImPares=0;
+    let i;
+    for (i=0;i<amountOfTours;i++){ 
+      const valordeCarater = parseInt( reverseCreditCardNumber.charAt(i)) ; 
+      if (i%2===0){
+        SumaValoresdePocisionesPares=SumaValoresdePocisionesPares+valordeCarater;
+      }
+      if (i%2===1){
+        let ValInpar;
+        ValInpar=valordeCarater*2;
+        if (ValInpar>9){//ValInpar = parseInt( (ValInpar.toString().charAt(0)))+ parseInt( (ValInpar.toString().charAt(1)));
+          ValInpar = ValInpar-9;
+        }
+        SumaValoresdePocisionesImPares=SumaValoresdePocisionesImPares+ValInpar;
+      }
+    }
+    const SumaTotal=SumaValoresdePocisionesImPares+SumaValoresdePocisionesPares;
+    const ValEnds= SumaTotal%10;
+    if (ValEnds===0){ 
+      return true;
+    } else {
+      return false;
+    }
+  }, 
+  maskify: (creditCardNumber) => {
+    // Oculta todos los dígitos de la tarjeta de crédito excepto los últimos 4
+    const length = creditCardNumber.length;
+    if(length<5){
+      return creditCardNumber;
+    }
+    return "#".repeat(length - 4) + creditCardNumber.substring(length - 4);
+  }
+}
 export default validator;
+
+ 
